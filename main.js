a32fa7c51616d1a923a7cf38014b554d0d25a47a
bayansamra <abusamrabayan@gmail.com>
2022-12-08 11:08:59 +0200

    add html , css and js files

---
+++ main.js
@@ -0,0 +1,11 @@
+const myDiv =document.getElementById('myDiv');
+const myBtn =document.querySelector('button');
+
+const array=["Hello","مرحبا"]
+myBtn.forEach((button) => {
+button.onclick=function(){
+localStorage.setItem=()=>{
+    
+}
+};
+});
