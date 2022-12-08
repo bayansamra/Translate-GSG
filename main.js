26a81ace56e71a4cb5264facfd39b620b64928f3
bayansamra <abusamrabayan@gmail.com>
2022-12-08 12:09:33 +0200

    modify html and js files

--- main.js
+++ main.js
@@ -1,11 +1,50 @@
-const myDiv =document.getElementById('myDiv');
 const myBtn =document.querySelector('button');
+const myDiv =document.querySelector('div');
 
-const array=["Hello","مرحبا"]
-myBtn.forEach((button) => {
-button.onclick=function(){
-localStorage.setItem=()=>{
-    
-}
+
+const langs = {
+    ar:{
+        buttonText:'en',
+        divText:'مرحبا ايها العالم',
+    },
+    en:{
+        buttonText:'العربية',
+        divText:'Hello World',
+        
+    },
 };
-});
+
+let lang =localStorage.getItem('lang') || 'en';
+
+const changeLangs =(lang)=>{
+    myBtn.textContent =langs[lang].buttonText;
+    myDiv.textContent=langs[lang].divText;
+
+// if(lang === 'en'){
+//     myBtn.textContent =langs.en.buttonText;
+//     myDiv.textContent=langs.en.divText;
+//     lang='ar';
+// } else {
+//     myBtn.textContent =langs.ar.buttonText;
+//     myDiv.textContent=langs.ar.divText;
+//     lang='en';
+// }  
+};
+
+
+const toggleLang = () =>{
+// let myNewLang=lang === 'en' ? 'ar' :'en';
+lang=lang === 'en' ? 'ar' :'en';
+
+    window.localStorage.setItem('lang', lang);
+changeLangs(lang);
+    // myBtn.textContent =langs[lang].buttonText;
+    // myDiv.textContent=langs[lang].divText;
+    };
+
+changeLangs(lang);
+
+myBtn.onclick = toggleLang;
+
+
+
